c247e2d82d1501d2f3801ab8fd02e2460f932e45
f-silvestre <silvestre.fernando.merced@gmail.com>
2025-02-27 09:21:57 +0100

    papaya

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,17 @@
 import java.io.*;
 import org.antlr.v4.runtime.*;
-/*
-El nombre ClasePrincipal es arbitrario, escoge el que prefieras.
-Sustituye Numbers por el nombre del fichero que contiene la especificación de la gramática ANTLR
-(extensión .g4)
-*/
+
 public class Main {
     public static void main(String[] args) {
         try{
-
-// Preparar el fichero de entrada para asignarlo al analizador léxico
             CharStream input = CharStreams.fromFileName(args[0]);
-// Crear el objeto correspondiente al analizador léxico con el fichero de
-// entrada
+
              transpilerLexer analex = new  transpilerLexer(input);
-// Identificar al analizador léxico como fuente de tokens para el
-// sintactico
+
             CommonTokenStream tokens = new CommonTokenStream(analex);
-// Crear el objeto correspondiente al analizador sintáctico
-             transpilerParser anasint = new  transpilerParser(tokens);
-/*
-Si se quiere pasar al analizador algún objeto externo con el que trabajar,
-este deberá ser de una clase del mismo paquete
-Aquí se le llama "sintesis", pero puede ser cualquier nombre.
-NumbersParser anasint = new NumbersParser(tokens, new sintesis());
-*/
-/*
-Comenzar el análisis llamando al axioma de la gramática
-Atención, sustituye "AxiomaDeLaGramatica" por el nombre del axioma de tu
-gramática
-*/
+
+            transpilerParser anasint = new  transpilerParser(tokens);
+
             anasint.prg();
         } catch (org.antlr.v4.runtime.RecognitionException e) {
 //Fallo al reconocer la entrada
